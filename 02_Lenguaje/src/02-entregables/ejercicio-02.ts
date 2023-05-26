887dd5febfffada37147d986b61edd37d2c9573a
gpla <germanps@outlook.es>
2023-05-26 12:11:01 +0200

    deliverable 2

--- 02_Lenguaje/src/02-entregables/ejercicio-02.ts
+++ 02_Lenguaje/src/02-entregables/ejercicio-02.ts
@@ -1,1 +1,14 @@
 console.log("************** DELIVERABLE 02 *********************");
+/*Implementa una función `concat` (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators.*/
+const concat = (a:number[], b: number[]) => [...a, ...b]; 
+console.log(concat([ 1, 2, 3 ] , [ 1, 2, 3]));
+
+/*Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2).*/
+const concatMultiple = (...arrayMultiple: any[]): any[] => {
+    let arrayConcated = [];
+    for(let item of arrayMultiple){
+        arrayConcated = [...arrayConcated, ...item]
+    }
+    return arrayConcated;
+}; 
+console.log(concatMultiple([ 1, false, 'House' ], [ 150, 0.25, 'index.html'], [ 'a', 'b', 'c'])); 
