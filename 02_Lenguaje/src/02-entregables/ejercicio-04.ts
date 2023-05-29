f8f555f19de90a6ceb6efd320468c09576883e70
gpla <germanps@outlook.es>
2023-05-29 08:22:13 +0200

    deliverable

--- 02_Lenguaje/src/02-entregables/ejercicio-04.ts
+++ 02_Lenguaje/src/02-entregables/ejercicio-04.ts
@@ -1,1 +1,26 @@
 console.log("************** DELIVERABLE 04 *********************");
+/*Crea una función isBookRead que reciba una lista de libros y un título y devuelva si se ha leído o no dicho libro.
+Un libro es un objeto con `title` como string y `isRead` como booleano. En caso de no existir el libro devolver `false`
+TIP: Existe un método de Array.prototype que te ayudará a buscar según un patrón.*/
+interface Book {
+    title: string;
+    isRead: boolean;
+}
+function isBookRead(books: Book[], titleToSearch: string) {
+    const book: Book = books.find(book => {
+        const { title } = book;
+        if (title === titleToSearch) return book;
+    })
+    if( book ) return book.isRead ? "Libro leído" : "Libro no leído"
+    else return false
+}
+const books: Book[] = [
+    {title: "Harry Potter y la piedra filosofal", isRead: true},
+    {title: "Canción de hielo y fuego", isRead: false},
+    {title: "Devastación", isRead: true},    
+];
+console.log(isBookRead(books, "Devastación")); // true
+console.log(isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
+//-------------------------OPCIONAL------------------------
+//Utiliza Typescript para añadir los tipos adecuados. OK
