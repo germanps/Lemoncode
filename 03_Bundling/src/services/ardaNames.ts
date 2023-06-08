9cb9120e45700fd59187d362bc463924d9453d05
gpla <germanps@outlook.es>
2023-06-08 19:25:48 +0200

    basic webpack exercise with react add

---
+++ 03_Bundling/src/services/ardaNames.ts
@@ -0,0 +1,12 @@
+import { names } from "../models";
+
+export const ardaNames: names[] = [
+    {id:0, name: 'Manwë'},
+    {id:1, name: 'Varda'},
+    {id:3, name: 'Melkor'},
+    {id:4, name: 'Mandos'},
+    {id:5, name: 'Yavanna'},
+    {id:6, name: 'Aulë'},
+    {id:7, name: 'Ulmo'},
+    {id:8, name: 'Tulkas'},
+]
