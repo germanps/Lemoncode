dc75fef70c0bff4af482ebbc26ca026f27daf69d
gpla <germanps@outlook.es>
2023-08-16 09:59:03 +0200

    app components & routes

--- 04_Frameworks/02_Angular/src/app/app.module.ts
+++ 04_Frameworks/02_Angular/src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Material components
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+//App Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/login/login.component';
+import { GalleryComponent } from './routes/gallery/gallery.component';
+import { AboutusComponent } from './routes/aboutus/aboutus.component';
+import { DashboardComponent } from './routes/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PrivateMenuComponent } from './components/private-menu/private-menu.component';
+import { PublicMenuComponent } from './components/public-menu/public-menu.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { CrudComponent } from './routes/crud/crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    GalleryComponent,
+    AboutusComponent,
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    PrivateMenuComponent,
+    PublicMenuComponent,
+    ProfileComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
