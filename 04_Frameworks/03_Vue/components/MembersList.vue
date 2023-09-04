31b8322b7a0e7f0268f2ee69affa0d347fee12ab
gpla <germanps@outlook.es>
2023-09-04 11:54:49 +0200

    composables members api call

--- 04_Frameworks/03_Vue/components/MembersList.vue
+++ 04_Frameworks/03_Vue/components/MembersList.vue
@@ -1,27 +1,65 @@
 <template>
-    <ul class="members-list">
-        <li class="members-list__item" v-for="member in members" :key="member.id">
-            <NuxtLink :to="`/member/${member.login}`" class="link"> 
-                <article class="member-item">
-                    <div class="img-wrapper">
-                        <img class="image" :src="member.avatar_url" />
-                    </div>
-                    <p class="paragraph">Nombre: {{ member.login }}</p>
-                    <p class="paragraph">Rol: {{ member.type }}</p>
-                </article>
-            </NuxtLink> 
-        </li>
-    </ul>
+    <div class="container-wrapper">
+        <form class="form">
+            <div class="search-wrapper">
+                <input type="text" v-model="companySearch" />
+                <button 
+                    class="button" 
+                    type="submit"
+                    @click.prevent="searchByCompany"
+                >Buscar</button>
+            </div>
+            <div class="total-members">
+                <span>Total: {{ totalMembers }}</span>
+            </div>
+
+        </form>
+        <ul class="members-list">
+            <li class="members-list__item" v-for="member in members" :key="member.id">
+                <NuxtLink :to="`/member/${member.login}`" class="link"> 
+                    <article class="member-item">
+                        <div class="img-wrapper">
+                            <img class="image" :src="member.avatar_url" />
+                        </div>
+                        <p class="paragraph">Nombre: {{ member.login }}</p>
+                        <p class="paragraph">Rol: {{ member.type }}</p>
+                    </article>
+                </NuxtLink> 
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup lang="ts">
-    import { membersService } from '@/services/members'
-    import { Member } from '~/types';
-    const members: Member[] = await membersService.get() 
- 
+    const { members, totalMembers } = await useMembersApi()
+    const companySearch = ref('lemoncode')
+
+    const searchByCompany = () => {
+        console.log(companySearch.value);
+    }
 </script>
 
 <style lang="scss" scoped>
+    .container-wrapper {
+        text-align: center;
+        margin: 1rem auto;
+        @media screen and (min-width: 550px) {
+            max-width: 550;
+        }
+        @media screen and (min-width: 768px) {
+            max-width: 768px;
+            
+        }
+        @media screen and (min-width: 1200px) {
+            max-width: 992px;
+            
+        }
+    }
+    .total-members {
+        width: 240px;
+        margin: auto;
+        text-align: left;
+    }
     .members-list {
         display: grid;
         grid-template-columns: repeat(1, 1fr);
